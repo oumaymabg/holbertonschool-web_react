5ecbcb44fe3c820e95c6e061ab6cb2b9921b519c
oumaymabg <oumaymabou257@gmail.com>
2021-11-12 15:24:56 +0100

    Advanced-type-part1_creating-functions-string-literal

--- 0x00-TypeScript/task_2/js/main.ts
+++ 0x00-TypeScript/task_2/js/main.ts
@@ -1,84 +1,84 @@
 interface DirectorInterface {
-  workFromHome(): string;
-  getCoffeeBreak(): string;
-  workDirectorTasks(): string;
-}
-
-interface TeacherInterface {
-  workFromHome(): string;
-  getCoffeeBreak(): string;
-  workTeacherTasks(): string;
-}
-
-export class Director implements DirectorInterface {
-  workFromHome(): string {
-    console.log("Working from home");
-    return "Working from home";
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workDirectorTasks(): string;
   }
-
-  getCoffeeBreak(): string {
-    console.log("Getting a coffee break");
-    return "Getting a coffee break";
+  
+  interface TeacherInterface {
+    workFromHome(): string;
+    getCoffeeBreak(): string;
+    workTeacherTasks(): string;
   }
-
-  workDirectorTasks(): string {
-    console.log("Getting to director tasks");
-    return "Getting to director tasks";
+  
+  export class Director implements DirectorInterface {
+    workFromHome(): string {
+      console.log("Working from home");
+      return "Working from home";
+    }
+  
+    getCoffeeBreak(): string {
+      console.log("Getting a coffee break");
+      return "Getting a coffee break";
+    }
+  
+    workDirectorTasks(): string {
+      console.log("Getting to director tasks");
+      return "Getting to director tasks";
+    }
   }
-}
-
-export class Teacher implements TeacherInterface {
-  workFromHome(): string {
-    return "Cannot work from home";
+  
+  export class Teacher implements TeacherInterface {
+    workFromHome(): string {
+      return "Cannot work from home";
+    }
+    getCoffeeBreak(): string {
+      return "Cannot have a break";
+    }
+    workTeacherTasks(): string {
+      return "Getting to work";
+    }
   }
-  getCoffeeBreak(): string {
-    return "Cannot have a break";
+  
+  export function createEmployee(salary: number | string): Teacher | Director {
+    if (typeof salary === "number" && salary < 500) return new Teacher();
+  
+    return new Director();
   }
-  workTeacherTasks(): string {
-    return "Getting to work";
+  
+  // console.log(createEmployee(200));
+  // console.log(createEmployee(1000));
+  // console.log(createEmployee("$500"));
+  
+  export function isDirector(employee: TeacherInterface | DirectorInterface): employee is Director {
+    return (employee as Director).workDirectorTasks !== undefined;
   }
-}
-
-export function createEmployee(salary: number | string): Teacher | Director {
-  if (typeof salary === "number" && salary < 500) return new Teacher();
-
-  return new Director();
-}
-
-// console.log(createEmployee(200));
-// console.log(createEmployee(1000));
-// console.log(createEmployee("$500"));
-
-export function isDirector(employee: TeacherInterface | DirectorInterface): employee is Director {
-  return (employee as Director).workDirectorTasks !== undefined;
-}
-
-export function executeWork(employee: DirectorInterface | TeacherInterface): string {
-  let msg;
-
-  if (isDirector(employee)) {
-    msg = employee.workDirectorTasks();
-  } else {
-    msg = employee.workTeacherTasks();
+  
+  export function executeWork(employee: DirectorInterface | TeacherInterface): string {
+    let msg;
+  
+    if (isDirector(employee)) {
+      msg = employee.workDirectorTasks();
+    } else {
+      msg = employee.workTeacherTasks();
+    }
+  
+    console.log(msg);
+    return msg;
   }
-
-  console.log(msg);
-  return msg;
-}
-
-// executeWork(createEmployee(300));
-// executeWork(createEmployee(1000));
-
-type Subjects = "Philo" | "Math";
-
-export function teachClass(todayClass:Subjects): string {
-  if (todayClass === "Philo") {
-    return "Teaching Philo";
-  } else if (todayClass === "Philo") {
-    return "Teaching Math";
+  
+  // executeWork(createEmployee(200));
+  // executeWork(createEmployee(1000));
+  
+  type Subjects = "Math" | "History";
+  
+  export function teachClass(todayClass:Subjects): string {
+    if (todayClass === "Math") {
+      return "Teaching Math";
+    } else if (todayClass === "History") {
+      return "Teaching History";
+    }
   }
-}
-
-// console.log(teachClass("philo"));
-
-// console.log(teachClass("Math"));
+  
+  // console.log(teachClass("Math"));
+  
+  // console.log(teachClass("History"));
