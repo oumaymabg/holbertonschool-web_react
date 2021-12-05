bfd90a5cc6af0a0edb99e9531913220dcfc26659
oumaymabg <oumaymabou257@gmail.com>
2021-12-05 12:38:08 +0100

    App-js

---
+++ 0x02-react_intro/task_2/dashboard/src/App.js
@@ -0,0 +1,25 @@
+import logo from './Holberton Logo.jpg';
+import './App.css';
+import { getFullYear, getFooterCopy } from './utils';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>School dashboard</h1>
+      </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+        <label for="email">Email: </label><input id="email" type="email"/>
+        <label for="password">Password: </label><input id="password" type="password"/>
+        <button id="buttom_ok">OK</button>
+      </body>
+      <footer className="App-footer">
+        <p>Copyright {getFullYear()} - {getFooterCopy(true)} </p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
